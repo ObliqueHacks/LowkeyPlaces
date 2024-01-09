cf802e3e37197248f15dc885464a8360632ff9ff
ObliqueHacks <74678940+ObliqueHacks@users.noreply.github.com>
2024-01-09 13:04:36 -0500

    almost done backend for login/register

--- backEnd/LowkeyPlaces/webApp/registrationAndLogin/views.py
+++ backEnd/LowkeyPlaces/webApp/registrationAndLogin/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from serializer import registrationSerializer, loginSerializer 
 # Create your views here.
+@api_view['Post']
+def register(request):
+    data = request.data
+    serializer=registrationSerializer(data=data)
+    if serializer.is_valid():
+        pass
+
+
+@api_view['Post']
+def login(request):
+    pass
+
+
+    
