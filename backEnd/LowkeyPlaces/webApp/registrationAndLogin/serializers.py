9c28acea35ae3fc792fa7a125164d5aa861de159
ObliqueHacks <74678940+ObliqueHacks@users.noreply.github.com>
2024-01-09 21:15:50 -0500

    backend for register is done

---
+++ backEnd/LowkeyPlaces/webApp/registrationAndLogin/serializers.py
@@ -0,0 +1,11 @@
+from .models import USER
+from rest_framework import serializers
+
+class registrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = USER
+        fields = ['id', 'name', 'psswd', 'email',]
+
+class loginSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=20)
+    psswd = serializers.CharField(max_lenth=30)
--- backEnd/LowkeyPlaces/webApp/registrationAndLogin/views.py
+++ backEnd/LowkeyPlaces/webApp/registrationAndLogin/views.py
@@ -2,16 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from serializer import registrationSerializer, loginSerializer 
+from .serializers import registrationSerializer, loginSerializer 
+from .models import USER
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
+from rest_framework import status
+
+
 # Create your views here.
-@api_view['Post']
 def register(request):
     data = request.data
-    serializer=registrationSerializer(data=data)
+    serializer = registrationSerializer(data=data)
     if serializer.is_valid():
-        pass
-
-
+        instance = serializer.save()
+        instance.dateTimeCreated = timezone.now()
+        instance.save()
+        return Response({'user': 'created'}, status=status.HTTP_201_CREATED)
+    else:
+        return Response({'user': 'not_created', 'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        
 @api_view['Post']
 def login(request):
     pass
