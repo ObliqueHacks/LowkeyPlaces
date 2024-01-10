642af6725377b350e6f7255b8833adee42ae22ef
ObliqueHacks <74678940+ObliqueHacks@users.noreply.github.com>
2024-01-10 11:32:16 -0500

    Revert "completed logic for login"
    This reverts commit 45680d588a2e085bbc1e693f867764491d6f3f32.

--- backEnd/LowkeyPlaces/webApp/registrationAndLogin/models.py
+++ backEnd/LowkeyPlaces/webApp/registrationAndLogin/models.py
@@ -5,7 +5,7 @@ class USER(models.Model):
     name=models.CharField(max_length = 20, unique=True)
     psswd=models.CharField(max_length=20)
     email=models.EmailField(unique=True)
-    dateTimeCreated=models.DateTimeField(auto_now_add=True)
+    dateTimeCreated=models.DateTimeField()
     isDeleted=models.BooleanField(default=False)
     mapCount=models.IntegerField(default=0)
     token=models.CharField(max_length = 15)
--- backEnd/LowkeyPlaces/webApp/registrationAndLogin/views.py
+++ backEnd/LowkeyPlaces/webApp/registrationAndLogin/views.py
@@ -7,49 +7,21 @@ from .models import USER
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from rest_framework import status
-from datetime import datetime, timedelta
-from django.db.models.query import QuerySet
-import jwt
-import secrets
 
-randomKey = secrets.token_hex(32)
-# generate token
-def create_token(elem: QuerySet[USER]):
-    # Define token payload
-    load = {
-        'user_id': elem.first()['USER'],
-        'exp': datetime.utcnow() + timedelta(days=3)  # Token expiration time
-    }
-    # Create and sign the token
-    token = jwt.encode(load, randomKey, algorithm='HS256')
-    return token
 
 # Create your views here.
 @api_view(['Post'])
 def register(request):
-    data=request.data
-    serializer=registrationSerializer(data=data)
+    data = request.data
+    serializer = registrationSerializer(data=data)
     if serializer.is_valid():
-        instance=serializer.save()
+        instance = serializer.save()
         instance.dateTimeCreated = timezone.now()
         instance.save()
-        return Response({'user': 'Successfully Registered'}, status=status.HTTP_201_CREATED)
+        return Response({'user': 'created'}, status=status.HTTP_201_CREATED)
     else:
-        return Response({'user': 'Username taken'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'user': 'not_created', 'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
         
 @api_view(['Post'])
 def login(request):
-    data = request.data
-    serializer = loginSerializer(data=data)
-    if serializer.is_valid():
-        serializer.save()
-        found=USER.objects.filter(field1=serializer.name, field2=serializer.psswd)
-        if found:
-            found=found.first()
-            genToken = create_token(found)
-            return Response({'genToken': genToken}, status=status.HTTP_201_CREATED)
-    return Response({'genToken': 'login or password incorrect'}, status=status.HTTP_201_CREATED)
-
-
-            
-
+    pass
