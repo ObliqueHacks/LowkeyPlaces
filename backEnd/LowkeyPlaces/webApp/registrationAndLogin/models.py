45680d588a2e085bbc1e693f867764491d6f3f32
ObliqueHacks <74678940+ObliqueHacks@users.noreply.github.com>
2024-01-10 11:31:06 -0500

    completed logic for login

--- backEnd/LowkeyPlaces/webApp/registrationAndLogin/models.py
+++ backEnd/LowkeyPlaces/webApp/registrationAndLogin/models.py
@@ -5,7 +5,7 @@ class USER(models.Model):
     name=models.CharField(max_length = 20, unique=True)
     psswd=models.CharField(max_length=20)
     email=models.EmailField(unique=True)
-    dateTimeCreated=models.DateTimeField()
+    dateTimeCreated=models.DateTimeField(auto_now_add=True)
     isDeleted=models.BooleanField(default=False)
     mapCount=models.IntegerField(default=0)
     token=models.CharField(max_length = 15)
